35b095b6861899e1441e7bb5ace4c21186f5af98
solo <syuleymandouble@gmail.com>
2021-08-23 19:08:32 +0200

    Initial commit

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,21 @@
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class ZippoTest {
+@Test
+    public void test()
+{
+    given()
+            //hazırlık işlemleri yapacağız
+
+            .when()
+            //link ve aksiyon işlemleri
+
+            .then()
+            //test ve extract işlemleri
+    ;
+}
+}
